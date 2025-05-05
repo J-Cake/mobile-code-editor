24b1520fbd79c0695ddb1b446b5e07777a2bf9c0
jcake <jakieschneider13@gmail.com>
2025-05-05 13:35:55 +0200

    Make modals close when you click outside of them

--- src/widgets/modal.tsx
+++ src/widgets/modal.tsx
@@ -3,19 +3,35 @@ import * as dom from 'react-dom/client';
 
 import style from "@css/popup.css?raw";
 
-export default class Modal {
+export default class Modal extends EventTarget {
+    renderer?: dom.Root;
+    root?: HTMLDialogElement;
+
     public show(children: React.ReactNode) {
-        const modal = document.querySelector('#modals')!
+        const modal = this.root = document.querySelector('#modals')!
             .appendChild(document.createElement('dialog'));
 
         modal.classList.add('modal');
 
-        dom.createRoot(modal)
-            .render(<>
-                <style scoped>{style}</style>
-                {children}
-            </>);
+        modal.addEventListener('click', e => {
+            if (e.target == modal)
+                this.close();
+        })
+
+        this.renderer = dom.createRoot(modal);
+
+        this.renderer.render(<>
+            <style scoped>{style}</style>
+            {children}
+        </>);
 
         modal.showModal();
+    }
+
+    close() {
+        this.root?.close();
+        this.root?.remove()
+        this.renderer?.unmount();
+        this.dispatchEvent(new Event('close'));
     }
 }
